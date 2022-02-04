0d3f934f501679997ebf8a9e8f9ae230014a6fff
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-04 15:35:14 +0700

    implemented clear board

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const container = document.getElementById('container');
-
-//container size : 30remx30rem
-
 let boardSize = 8;
+container.style.gridTemplateColumns = 'repeat(' + boardSize + ', 1fr)';
 
-for (let i = 0; i < boardSize ** 2; i++) {
-    const element = document.createElement('div');
-
-    element.textContent = i+1;
-    element.style.fontSize = '7px';
-
-    element.onmouseenter = () => element.style.background = 'whitesmoke';
-
-    container.appendChild(element);
+function fillBoard() {
+    for (let i = 0; i < boardSize ** 2; i++) {
+        const element = document.createElement('div');
+    
+        element.textContent = i+1;
+        element.style.fontSize = '7px';
+    
+        element.onmouseenter = () => element.style.background = 'whitesmoke';
+    
+        container.appendChild(element);
+    }
 }
 
-container.style.gridTemplateColumns = 'repeat(' + boardSize + ', 1fr)';
+function clearBoard() {
+    console.log("clearing the board!!!");
+    for (let i = 0; i < boardSize ** 2; i++) {
+        container.removeChild(container.firstChild);
+    }
+    fillBoard();
+}
+
+fillBoard();
+
+const btnClear = document.getElementById('btnClear');
+btnClear.addEventListener('click', clearBoard);
